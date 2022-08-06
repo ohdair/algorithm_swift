72d9aa49961d50318d76f4e6d62b10335fd758ab
ohdair <ohdiar@gmail.com>
2022-08-06 21:40:57 +0900

    Add Programmers 67256 í°³

--- programmers 67256 1lvl/solution.swift
+++ programmers 67256 1lvl/solution.swift
@@ -1,3 +1,4 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/67256
 import Foundation
 
 func solution(_ numbers:[Int], _ hand:String) -> String {
@@ -8,34 +9,32 @@ func solution(_ numbers:[Int], _ hand:String) -> String {
     for i in 1...9 {
         datas.append([(i - 1) / 3, (i - 1) % 3])
     }
-    func distance(_ N:[Int], _ L:[Int], _ R:[Int]) -> String {
+    func distance(_ N: inout [Int], _ L: inout [Int], _ R: inout [Int]) -> String {
         let leftD:Int = abs(N[0] - L[0]) + abs(N[1] - L[1])
         let rightD:Int = abs(N[0] - R[0]) + abs(N[1] - R[1])
         if leftD == rightD && hand == "right" {
+            R = N
             return "R"
         } else if leftD == rightD && hand == "left" {
+            L = N
             return "L"
         } else if leftD > rightD {
+            R = N
             return "R"
         }
+        L = N
         return "L"
     }
     for number in numbers {
-        if number == 2 || number == 5 || number == 8 || number == 0 {
-            let tmp:String = distance(datas[number], leftHand, rightHand)
-            if tmp == "L" {
-                leftHand = datas[number]
-            } else {
-                rightHand = datas[number]
-            }
-            ret += tmp
-        } else if number == 1 || number == 4 || number == 7 {
+        if number % 3 == 1 {
             leftHand = datas[number]
             ret += "L"
-        } else {
+        } else if number != 0 && number % 3 == 0 {
             rightHand = datas[number]
             ret += "R"
-        }
+        } else {
+            ret += distance(&datas[number], &leftHand, &rightHand)
+        } 
     }
     return ret
 }
